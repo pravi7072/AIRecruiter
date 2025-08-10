9bc54564ee886011318fd139eb79739b52cd1e05
pravi7072 <pravinthete427@gmail.com>
2025-08-10 23:54:20 +0530

    added

--- app/verify/Verified.jsx
+++ app/verify/Verified.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
-export default function Verify() {
+export default function Verified() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
   const [message, setMessage] = useState("Verifying...");
--- app/verify/page.js
+++ app/verify/page.js
@@ -1,12 +1,12 @@
 'use client';
 
 import { Suspense } from "react";
-import Verify from "./Verify";
+import Verified from "./Verified";
 
 export default function VerifyPage() {
   return (
     <Suspense fallback={<div className="text-center mt-20 text-xl">Loading...</div>}>
-      <Verify />
+      <Verified />
     </Suspense>
   );
 }
